fcda0792e612e67ca203afedc89f0a089440f059
s5443626 <117823998+s5443626@users.noreply.github.com>
2022-11-14 14:46:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    float a=0, b=0, x=0, y=0;
+int main() {    
+    float a=0, b=0, x=0, y=0,z=0;
     cin >> a >> b >> x >> y;
-    float z=0;
-    if(x<0,y>0){
-        z=(a*x-b*y);
-    } else if(x>=0,y<=0){
-        z=(a*x*x-b*y);
+    if((x<0)&&(y>0)){
+        z=(a*x)-(b*y);
+    } else if((x>=0)&&(y<=0)){
+        z=(a*x*x)-(b*y);
     } else{
-        z=(a*x+b*y*y);
+        z=(a*x)+(b*y*y);
     }
     cout << z << endl;
-   return 0;
+    return 0;
 }
